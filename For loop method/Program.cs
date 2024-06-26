e04bd1bd2441a69010f34d7d59963ef1b7ee0d0c
Todor Popov <todorapopov@outlook.com>
2024-06-26 08:39:59 +0300

    Add project files.

---
+++ For loop method/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace For_loop_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            static void Loop(int i, char k)
+            {
+
+
+                for (int n = i; n >= 0; n--)
+                {
+
+                    for (int j = 0; j <= n; j++)
+                    {
+                        Console.Write(k);
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+
+
+            Loop(6,'r');
+        }
+    }
+}
+/*
+******
+*****
+****
+***
+**
+*
+*/
